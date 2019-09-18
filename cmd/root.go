28778c92ded3675055e0bafc201161464195cedb
Valerio Iachini <valerio.iachini@gmail.com>
2019-09-18 21:31:49 +0200

    Update short description

--- cmd/root.go
+++ cmd/root.go
@@ -34,16 +34,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gitlab-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Command line interface for gitlab",
+	Long:  ``,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		if viper.GetBool("gitlab.insecure") {
 			http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
